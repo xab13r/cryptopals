7b0c131699ae29f895a0f2ef8a71be4009ee4dfb
xab13r <xabier.nk@me.com>
2018-12-03 17:02:34 +0000

    Code cleanup

--- set01challenge02.py
+++ set01challenge02.py
@@ -9,7 +9,6 @@ FIXED XOR
 
 import codecs
 import base64
-from set01challenge01 import hex_to_bytes
 
 string1 = "1c0111001f010100061a024b53535009181c"
 string2 = "686974207468652062756c6c277320657965"
