e3bcb6e9a2b3774680da949294d91cdf7c746f94
xab13r <xabier.nk@me.com>
2018-12-08 19:31:39 +0000

    Re-wrote xor_string function

--- set01challenge02.py
+++ set01challenge02.py
@@ -7,8 +7,7 @@ FIXED XOR
 
 '''
 
-import codecs
-import base64
+import binascii
 
 string1 = "1c0111001f010100061a024b53535009181c"
 string2 = "686974207468652062756c6c277320657965"
@@ -16,24 +15,34 @@ expected_result = "746865206b696420646f6e277420706c6179"
 
 
 def xor_strings(string1, string2):
-    int_string1 = int(string1, 16)  # XOR operation on INT
-    int_string2 = int(string2, 16)  # XOR operation on INT
 
-    if len(string1) != len(string2):
-        raise Exception("Error - Strings length doesn\' match")
+    # Make strings the same length
+    if len(string1) > len(string2):
+        string2 *= int(len(string1)/len(string2)+1)
+        string2 = string2[:len(string1)]
 
-    else:
-        xor_string = int_string1 ^ int_string2
+    if len(string1) < len(string2):
+        string1 *= int(len(string2)/len(string1)+1)
+        string1 = string1[:len(string2)]
 
-    return hex(xor_string)[2:]  # Remove '0x' prefix from hex encoding
+    bytes_string1 = binascii.unhexlify(string1)  # Convert to type bytes
+    bytes_string2 = binascii.unhexlify(string2)  # Convert to type bytes
+
+    xor_string = b''
+    for i in range(len(bytes_string1)):
+        xor_string += bytes([bytes_string1[i] ^ bytes_string2[i]])
+
+    return xor_string # Value returned is type bytes
 
 
 def main():
+    
     print(string1)
     print(string2)
-    print(expected_result)
-
-    if xor_strings(string1, string2) != expected_result:
+    
+    print(binascii.hexlify(xor_strings(string1, string2)))
+    
+    if xor_strings(string1, string2) != binascii.unhexlify(expected_result):
         raise Exception("Error - Strings don\'t match")
     else:
         print("--- Success ---")
