4eb32c8f476812076032d36b7d0db980a83e717a
xab13r <xabier.nk@me.com>
2018-12-30 19:04:38 +0000

    Formatting changes

--- set02challenge11.py
+++ set02challenge11.py
@@ -6,6 +6,7 @@ Set 02 - Challenge 11
 AN ECB/CBC DETECTION ORACLE
 
 '''
+
 import base64
 from Crypto.Cipher import AES
 from Crypto.Util.strxor import strxor
@@ -18,12 +19,13 @@ from set02challenge10 import cbc_module
 key = b"YELLOW SUBMARINE"
 iv = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 
+
 class ecb_module():
 
     def __init__(self):
         # Create cipher instance
         self.cipher = AES.new(key, AES.MODE_ECB)
-    
+
     # ECB Encryption Method
     def ecb_encrypt(self, plaintext, key):
         # Slice plaintext into blocks
@@ -48,18 +50,17 @@ class ecb_module():
         # Decrypt each block and add it to the plaintext list
         for i in range(len(ciphertext_blocks)):
             plaintext_blocks[i] = self.cipher.decrypt(ciphertext_blocks[i])
-        
+
         # Get last block and identify padding
         padding = plaintext_blocks[-1][-1]
         # Remove padding from last block
         plaintext_blocks[-1] = plaintext_blocks[-1][:-padding]
         # Initialize plaintext variable
         plaintext = b""
-        # Add blocks together 
+        # Add blocks together
         for i in plaintext_blocks:
             plaintext += i
         return plaintext
-
 
 
 def encryption_oracle(plaintext):
@@ -67,9 +68,9 @@ def encryption_oracle(plaintext):
     random_iv = get_random_bytes(16)
     # print("Random key:", random_key)
     # print("Random iv:", random_iv)
-    random_length = randint(5,10)
+    random_length = randint(5, 10)
     # print("Random length addition:",random_length)
-    random_addition = get_random_bytes(random_length) 
+    random_addition = get_random_bytes(random_length)
     # print("Random addition:", random_addition)
     plaintext = random_addition + plaintext + random_addition
     # print("Plaintext:", plaintext)
@@ -80,7 +81,7 @@ def encryption_oracle(plaintext):
     ciphertext = b""
     cipher_ecb = ecb_module()
     cipher_cbc = cbc_module(random_iv, random_key)
-    
+
     if choice == 1:
         print("ECB")
         ciphertext = cipher_ecb.ecb_encrypt(plaintext, random_key)
@@ -93,7 +94,7 @@ def encryption_oracle(plaintext):
 
 
 def main():
-    
+
     # Chosen ciphertext attack
     # Create a 3 blocks of 0x00. This way when the oracle is encrypting the plaintext in ECB mode the second and third block will be the same
     oracle = encryption_oracle(bytes([0]*48))
