cf268ed16af84ea821cab775f92f674b27ae6fa7
xab13r <xabier.nk@me.com>
2019-02-20 07:59:16 +0000

    Using new xor_strings function

--- set01challenge05.py
+++ set01challenge05.py
@@ -18,17 +18,17 @@ key = b'ICE'
 expected_result = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
 
 
-def repeating_key_xor(input_string, key):
-    # Make input_strings and key the same length
-    if len(input_string) > len(key):
-        key *= int(len(input_string)/len(key)+1)
-        key = key[:len(input_string)]
+def repeating_key_xor(string1, string2):
+    # Make string1s and string2 the same length
+    if len(string1) > len(string2):
+        string2 *= int(len(string1)/len(string2)+1)
+        string2 = string2[:len(string1)]
 
-    if len(input_string) < len(key):
-        input_string *= int(len(key)/len(input_string)+1)
-        input_string = string1[:len(key)]
+    if len(string1) < len(string2):
+        string1 *= int(len(string2)/len(string1)+1)
+        string1 = string1[:len(string2)]
     
-    return xor_strings(input_string, key)
+    return xor_strings(string1, string2)
 
 
 def main():
